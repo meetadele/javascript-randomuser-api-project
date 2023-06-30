6627504357d5dcb3393b8c9c04c4fae84f46e3ed
meetadele <kovacsadel85@gmail.com>
2023-06-30 00:12:00 -0400

    Randomuser API basic project setup

---
+++ randomapi.js
@@ -0,0 +1,25 @@
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const results = document.querySelector("[name=results]").value;
+  const nat = document.querySelector("[name=nat]").value;
+
+  fetch(`https://randomuser.me/api/?results=${results}&nat=${nat}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const userList = data.results;
+      const markup = userList
+        .map(
+          (user) => `
+        <div class="user m-2">
+            <img src="${user.picture.large}" alt="Avatar of ${user.name.first} ${user.name.last}"/>
+        <div>${user.name.first} ${user.name.last}</div>
+        <div>${user.email}</div>
+        <div>${user.location.city}, ${user.location.country}</div>
+        </div>
+      `
+        )
+        .join("");
+      document.querySelector(".js-users").innerHTML = markup;
+    });
+});
